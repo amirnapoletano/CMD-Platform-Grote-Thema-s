31d24dc888cb1eceb3894aa1f66122f724dadd72
Amir Napoletano <amirnapoletano@gmail.com>
2024-04-07 02:26:44 +0200

    🎉 Aikido commit.

---
+++ src/lib/Components/Footer.svelte
@@ -0,0 +1,23 @@
+<footer>
+    &#169; {new Date().getFullYear()} —
+	<a href="https://www.hva.nl/" target="_blank" rel="noopener noreferrer">Hogeschool van Amsterdam</a>
+</footer>
+
+<style>
+	footer {
+		background-color: var(--color-hva-navy);
+		margin: 2rem auto 0;
+		padding: 1rem;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+	}
+
+    @media (min-width: 48rem) {
+        footer {
+            margin: 0 auto;
+            position: relative;
+        }
+    }
+</style>
